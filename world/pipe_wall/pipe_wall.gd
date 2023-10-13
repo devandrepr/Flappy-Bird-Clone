aebc9070cc210a2473b3625f56def8f511b947c0
devandrepr <devandrepr+git@gmail.com>
2023-10-13 16:01:58 -0300

    Change types of PipeTop and PipeBottom to Area2D, abandon Pipe class
    Make collision detections and Player death execution a bit simpler.

--- world/pipe_wall/pipe_wall.gd
+++ world/pipe_wall/pipe_wall.gd
@@ -3,22 +3,25 @@ extends Node2D
 
 @export var speed: int = -24*3
 
-@onready var pipe_bottom: Pipe = $PipeBottom
-@onready var pipe_top: Pipe = $PipeTop
+@onready var pipe_bottom: Area2D = $PipeBottom
+@onready var pipe_top: Area2D = $PipeTop
 @onready var _score_area: Area2D = $ScoreArea
 @onready var _vos: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
 
 
 func _ready() -> void:
-	_score_area.connect("body_entered", _on_body_entered_score_area)
-	_vos.connect("screen_exited", _on_screen_exited)
-
-	pipe_bottom.opposite_pipe = pipe_top
-	pipe_top.opposite_pipe = pipe_bottom
+	pipe_bottom.body_entered.connect(_on_body_entered_pipe.bind(pipe_bottom, pipe_top))
+	pipe_top.body_entered.connect(_on_body_entered_pipe.bind(pipe_top, pipe_bottom))
+	_score_area.body_entered.connect(_on_body_entered_score_area)
+	_vos.screen_exited.connect(_on_screen_exited)
 
 
 func _process(delta: float) -> void:
 	position.x += speed * delta
+
+
+func _on_body_entered_pipe(body: Node2D, pipe: Area2D, other_pipe: Area2D) -> void:
+	GameManager.body_entered_pipe.emit(body, pipe, other_pipe)
 
 
 func _on_body_entered_score_area(_body: Node2D) -> void:
